92d0aa4033257d71d639b9c3159ffd7a70249cc3
Mo <derhallim@gmail.com>
2017-10-08 13:57:01 -0400

    sdfasdf

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,20 @@
-$(document).ready(function() {
-    $('.nav li.dropdown').hover(function() {
-        $(this).addClass('open');
-    }, function() {
-        $(this).removeClass('open');
-    });
-});
+// $(document).ready(function() {
+//     $('.nav li.dropdown').hover(function() {
+//         $(this).addClass('open');
+//     }, function() {
+//         $(this).removeClass('open');
+//     });
+// });
 
+$('.dropdown').on('show.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
 
+  // Add slideUp animation to Bootstrap dropdown when collapsing.
+  $('.dropdown').on('hide.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
+  
 window.onresize = function(){
     console.log(document.documentElement.clientWidth)
 }
