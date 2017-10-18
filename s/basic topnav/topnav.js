a8c316933ff5e1cdc4b353cd46fb29d54fb31e48
Mo <derhallim@gmail.com>
2017-10-18 15:54:54 -0400

    commit

--- s/basic topnav/topnav.js
+++ s/basic topnav/topnav.js
@@ -1,5 +1,17 @@
 var tabs= document.getElementsByClassName("nav");
+clearClasses();
 
-for(var i=0; i<tabs.length;i++){    
-        tabs[i].className = tabs[i].className.replace(" active", "");    
+for(var i=0; i< tabs.length ; i++){    
+        tabs[i].onclick = function(){
+                clearClasses();
+                this.classList.add("active");
+        }
+}
+
+      
+
+function clearClasses(){
+        for(var i=0; i<tabs.length;i++){    
+                        tabs[i].className = tabs[i].className.replace(" active", "");    
+        }
 }
